31f7d52189e155cabbf2b82b7d3407c771646aa9
raejoonee <crj0901@gmail.com>
2021-08-04 17:29:47 +0900

    feat: 새로 만든 컴포넌트들을 App.js와 index.js에 적용한다

--- src/App.js
+++ src/App.js
@@ -1,53 +1,80 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
 import SearchSection from './components/SearchSection.js';
-import ResultSection from './components/ResultSection.js';
+import DescriptionSection from './components/DescriptionSection.js';
+import GallerySection from './components/GallerySection.js';
 import Loader from './components/Loader.js';
-import { api } from './api/TheCatAPI.js';
+import api from './api/TheCatAPI.js';
+import Toast from './components/Toast.js';
 
 export default class App {
   constructor($app) {
-    this.$app = $app;
-    this.state = {
+    this.$state = {
       isLoading: true,
-      result: null,
+      breedData: null,
+      imageData: null,
       breeds: new Map(),
     };
 
-    const searchRandomCat = () => {
-      const response = api.getRandomCats();
-      response.then(data => {
-        resultSection.setState(data);
-      });
-    }
-  
-    const searchSpecificCat = id => {
-      const response = api.getSpecificCats(id);
-      response.then(data => {
-        resultSection.setState(data);
-      });
-    }
-    
-    const loadingSection = new Loader({
-      $app: this.$app,
-      isLoading: this.state.isLoading
+    const loader = new Loader({
+      $app,
+      isLoading: this.$state.isLoading,
     });
+
+    const setLoaderState = (nextState) => {
+      loader.setState(nextState);
+    };
+
+    const searchRandomCat = () => {
+      setLoaderState(true);
+      const response = api.getRandomCats();
+      response.then((data) => {
+        gallerySection.setState(data);
+      });
+      setLoaderState(false);
+    };
+
+    const searchSpecificCat = (id) => {
+      setLoaderState(true);
+      let breedSwitch = true;
+      let imageSwitch = true;
+      const breedResponse = api.getBreedInfo(id);
+      breedResponse.then((nextState) => {
+        descriptionSection.setState(nextState);
+        if (!imageSwitch) setLoaderState(false);
+        breedSwitch = false;
+      });
+      const imageResponse = api.getSpecificCats(id);
+      imageResponse.then((data) => {
+        gallerySection.setState(data);
+        if (!breedSwitch) setLoaderState(false);
+        imageSwitch = false;
+      });
+    };
+
+    const setToastMessage = (nextState) => {
+      toast.setState(nextState);
+    };
 
     const searchSection = new SearchSection({
-      $app: this.$app,
-      breeds: this.state.breeds,
-      result: this.state.result,
-      isLoading: this.isLoading,
+      $app,
+      breeds: this.$state.breeds,
+      result: this.$state.imageData,
+      isLoading: this.$state.isLoading,
       searchRandomCat,
-      searchSpecificCat
+      searchSpecificCat,
+      setLoaderState,
+      setToastMessage,
     });
 
-    const resultSection = new ResultSection({
-      $app: this.$app,
-      result: this.state.result,
-    });
-  }
+    const descriptionSection = new DescriptionSection($app);
 
-  setState(nextState) {
-    this.state = nextState;
-    this.render();
+    const gallerySection = new GallerySection($app);
+
+    const toast = new Toast({
+      $app,
+      message: '',
+      toggle: false,
+    });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import App from './App.js';
 import './style/style.css';
 import './style/Loader.css';
+import './style/Toast.css';
+import './style/Description.css';
 
+// eslint-disable-next-line no-unused-vars
 const app = new App(document.querySelector('#app'));
