5a2220acd1997d87bf37a432dc471d2f937f0b54
raejoonee <crj0901@gmail.com>
2021-08-04 17:27:53 +0900

    feat: SearchSection 컴포넌트와 Toast, Loader 컴포넌트 간 의존성을 만들고 기능을 구현한다

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -1,20 +1,30 @@
-import { api } from "../api/TheCatAPI.js";
+import api from '../api/TheCatAPI.js';
 
 export default class SearchSection {
-  constructor({ $app, breeds, result, isLoading, searchRandomCat, searchSpecificCat }) {
-    this.$app = app;
+  constructor({
+    $app,
+    breeds,
+    result,
+    isLoading,
+    searchRandomCat,
+    searchSpecificCat,
+    setLoaderState,
+    setToastMessage,
+  }) {
     this.state = { breeds, result, isLoading };
     this.onSearch = { searchRandomCat, searchSpecificCat };
-
+    this.setLoaderState = setLoaderState;
+    this.setToastMessage = setToastMessage;
     this.$target = document.createElement('section');
     this.$target.className = 'search-section';
     $app.appendChild(this.$target);
-
-    api.getBreeds().then(data => {
-      for (let breed of data) {
+    api.getBreeds().then((data) => {
+      data.forEach((breed) => {
+        // if (!breed.name.includes(' ') && !breed.name.includes('-'))
         this.state.breeds.set(breed.name, breed.id);
-      }
+      });
       this.render();
+      setLoaderState(false);
     });
   }
 
@@ -24,10 +34,10 @@ export default class SearchSection {
   }
 
   render() {
-    this.$target.innerHTML = "";
+    this.$target.innerHTML = '';
     const label = document.createElement('label');
     label.setAttribute('for', 'cat-breed-choice');
-    // label.textContent = '고양이 종을 선택해주세요:';
+    label.textContent = '고양이 종을 선택해주세요:';
 
     const selectBox = document.createElement('input');
     selectBox.id = 'select-box';
@@ -36,29 +46,46 @@ export default class SearchSection {
     selectBox.autofocus = true;
     selectBox.placeholder = 'Pick a breed';
 
-    selectBox.addEventListener('keypress', e => {
-      if (e.key === 'Enter' && selectBox.value) {
-        this.searchRandomCat();
+    selectBox.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        const { value } = selectBox;
+        const convertedValue = value[0].toUpperCase() + value.slice(1);
+        if (this.state.breeds.get(convertedValue)) {
+          this.onSearch.searchSpecificCat(
+            this.state.breeds.get(convertedValue),
+          );
+          selectBox.value = '';
+        } else {
+          this.setToastMessage({
+            message: '검색어를 정상적으로 입력해주세요.',
+            toggle: true,
+          });
+        }
       }
     });
-    
+
     const datalist = document.createElement('datalist');
     datalist.id = 'cat-list';
 
-    for (let breed of this.state.breeds) {
+    this.state.breeds.forEach((id, name) => {
       const option = document.createElement('option');
-      option.value = breed[0];
+      option.value = name;
       datalist.appendChild(option);
-    }
+    });
 
     const button = document.createElement('button');
     button.textContent = '검색';
-    button.addEventListener('click', e => {
-      e.preventDefault();
-      if (selectBox.value && this.state.breeds.get(selectBox.value)) {
-        this.onSearch.searchSpecificCat(this.state.breeds.get(selectBox.value));
+    button.addEventListener('click', () => {
+      const { value } = selectBox;
+      const convertedValue = value[0].toUpperCase() + value.slice(1);
+      if (this.state.breeds.get(convertedValue)) {
+        this.onSearch.searchSpecificCat(this.state.breeds.get(convertedValue));
+        selectBox.value = '';
       } else {
-        alert('검색어를 정상적으로 입력해주세요.');
+        this.setToastMessage({
+          message: '검색어를 정상적으로 입력해주세요.',
+          toggle: true,
+        });
       }
     });
 
