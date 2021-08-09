557f3d82fdbeec421eb868b235dcadac2ca29da3
raejoonee <crj0901@gmail.com>
2021-08-09 17:42:50 +0900

    chore : App.js의 가독성 추가 향상

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
-/* eslint no-unused-expressions: [{ allowTernary: true }] */
+/* eslint no-unused-expressions: [2, { allowTernary: true }] */
 import SearchSection from './components/SearchSection.js';
 import ResultSection from './components/ResultSection.js';
 import Loader from './components/Loader.js';
@@ -8,12 +8,8 @@ import DarkMode from './components/DarkMode.js';
 
 export default class App {
   constructor($app) {
-    const isLightMode = localStorage.getItem('isLightMode') === 'true';
-    if (isLightMode) {
-      document.querySelector('body').classList.remove('dark');
-    } else {
-      document.querySelector('body').classList.add('dark');
-    }
+    this.isLightMode = localStorage.getItem('isLightMode') === 'true';
+    this.setDarkMode('body');
 
     const searchSection = new SearchSection($app, {
       setLoaderState: (nextState) => {
@@ -27,14 +23,12 @@ export default class App {
 
     const darkMode = new DarkMode($app);
 
-    isLightMode
-      ? document.querySelector('.dark-mode-section').classList.remove('dark')
-      : document.querySelector('.dark-mode-section').classList.add('dark');
+    this.setDarkMode('.dark-mode-section');
+  }
 
-    if (isLightMode) {
-      document.querySelector('.dark-mode-section').classList.remove('dark');
-    } else {
-      document.querySelector('.dark-mode-section').classList.add('dark');
-    }
+  setDarkMode($target) {
+    this.isLightMode
+      ? document.querySelector($target).classList.remove('dark')
+      : document.querySelector($target).classList.add('dark');
   }
 }
